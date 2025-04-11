bae1bb02a0b693c83a2449bfcfea5b0ceba54cc8
Alan Philip <alanphilipdev@gmail.com>
2025-04-11 10:41:50 +0200

    add subgraph for custodial

--- src/pix-offchain-custodial.ts
+++ src/pix-offchain-custodial.ts
@@ -1,70 +1,30 @@
 import {
   Deposited as DepositedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  Paused as PausedEvent,
-  Unpaused as UnpausedEvent,
   WithdrawRequested as WithdrawRequestedEvent,
   Withdrawn as WithdrawnEvent
 } from "../generated/PIXOffchainCustodial/PIXOffchainCustodial"
 import {
   Deposited,
-  OwnershipTransferred,
-  Paused,
-  Unpaused,
   WithdrawRequested,
-  Withdrawn
+  Withdrawn,
+  Account
 } from "../generated/schema"
 
 export function handleDeposited(event: DepositedEvent): void {
   let entity = new Deposited(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.depositor = event.params.depositor
+
+  let account = Account.load(event.params.depositor)
+  if (!account) {
+    account = new Account(event.params.depositor)
+    account.save()
+  }
+
+  entity.depositor = account.id
   entity.amount = event.params.amount
   entity.timestamp = event.params.timestamp
   entity.nonce = event.params.nonce
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePaused(event: PausedEvent): void {
-  let entity = new Paused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleUnpaused(event: UnpausedEvent): void {
-  let entity = new Unpaused(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.account = event.params.account
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
@@ -77,7 +37,14 @@ export function handleWithdrawRequested(event: WithdrawRequestedEvent): void {
   let entity = new WithdrawRequested(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.withdrawer = event.params.withdrawer
+
+  let account = Account.load(event.params.withdrawer)
+  if (!account) {
+    account = new Account(event.params.withdrawer)
+    account.save()
+  }
+
+  entity.withdrawer = account.id
   entity.amount = event.params.amount
   entity.timestamp = event.params.timestamp
   entity.nonce = event.params.nonce
@@ -93,7 +60,14 @@ export function handleWithdrawn(event: WithdrawnEvent): void {
   let entity = new Withdrawn(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.withdrawer = event.params.withdrawer
+
+  let account = Account.load(event.params.withdrawer)
+  if (!account) {
+    account = new Account(event.params.withdrawer)
+    account.save()
+  }
+
+  entity.withdrawer = account.id
   entity.amount = event.params.amount
   entity.timestamp = event.params.timestamp
   entity.nonce = event.params.nonce
